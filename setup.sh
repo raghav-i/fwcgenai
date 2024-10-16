bb80b75ed4ce3e2f46ee5ca7b4db99345e09d8f1
raghav-i <raghavendradabral@gmail.com>
2024-10-17 00:54:11 +0530

    setup changes

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@ mamba create -n myenv python=3.8 -y
 source activate myenv
 
 # Install required packages
-pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu118
+pip install opencv-python torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu118
 pip install timm einops albumentations matplotlib tensorboard jax accelerate
 pip install diffusers ftfy madgrad openai-clip numpy transformers
-pip install Flask pyngrok
+pip install flask pyngrok
 
